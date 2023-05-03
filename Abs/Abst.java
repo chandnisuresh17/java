e15027014bf3a564d5f7c06012ea20ea893ae33c
chandnisuresh17 <chandnisuresh2000@gmail.com>
2023-05-03 11:43:27 +0530

    abst

---
+++ Abs/Abst.java
@@ -0,0 +1,33 @@
+package Abs;
+
+abstract class Animal{
+    abstract public void sound();
+        
+    
+    public void Dog(){
+        System.out.println("Bark");
+    }
+}
+class Tiger extends Animal{
+    public void sound(){
+    System.out.println("Roar");
+    }
+}
+class Cat extends Animal{
+    public void sound(){
+        System.out.println("Meow");
+    }
+
+}
+
+public class Abst{
+    public static void main(String[] args){
+        Animal a = new Cat();
+        Animal a1 = new Tiger();
+        a.sound();
+       
+        
+
+
+    }
+}
